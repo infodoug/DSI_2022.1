4f305ce6eecf46f516495fde7d4bf1883ffe92e8
infodoug <douglasrafael.info@gmail.com>
2022-12-05 19:39:36 -0300

    add função de favoritar e visualização em cards

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -16,39 +16,187 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-        ),
-        body: const Center(
-          child: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
         ),
       ),
+      home: const RandomWords(),
     );
   }
 }
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+  int _viewNumber = 0;
+
+  chooseView(choosenMode) {
+    if (choosenMode == 0) {
+      return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          final alreadySaved = _saved.contains(_suggestions[index]);
+          return ListTile(
+              title: Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+              ),
+              onTap: () {
+                // NEW from here ...
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                }); // to here.
+              });
+        },
+      );
+    }
+    if (choosenMode == 1) {
+      return GridView.builder(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        // Generate 100 widgets that display their index in the List.
+        //itemCount: _suggestions.length,
+        itemBuilder: (context, i) {
+          final index = i;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+
+          final alreadySaved = _saved.contains(_suggestions[index]);
+          return Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                height: 220,
+                width: double.maxFinite,
+                child: Card(
+                  elevation: 5,
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: Align(
+                        child: IconButton(
+                          icon: Icon(
+                            alreadySaved
+                                ? Icons.favorite
+                                : Icons.favorite_border,
+                          ),
+                          color: alreadySaved ? Colors.red : null,
+                          tooltip: alreadySaved ? 'Remove from saved' : 'Save',
+                          onPressed: (() {
+                            setState(() {
+                              if (alreadySaved) {
+                                _saved.remove(_suggestions[index]);
+                              } else {
+                                _saved.add(_suggestions[index]);
+                              }
+                            });
+                          }),
+                        ),
+                        alignment: Alignment.topRight,
+                      )),
+                      Expanded(
+                          child: Align(
+                        child: Text(
+                          _suggestions[index].asPascalCase,
+                          style: _biggerFont,
+                        ),
+                        alignment: Alignment.center,
+                      )),
+                      Expanded(child: Container())
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void _pushSaved() {
+    Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.view_carousel),
+            onPressed: () {
+              if (_viewNumber == 0) {
+                setState(() {
+                  _viewNumber++;
+                });
+              } else if (_viewNumber == 1) {
+                setState(() {
+                  _viewNumber--;
+                });
+              }
+            },
+            tooltip: 'Change to Card View',
           ),
-        );
-      },
+          IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          )
+        ],
+      ),
+      body: chooseView(_viewNumber),
     );
   }
 }
